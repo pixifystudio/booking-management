c778790a748892499419456143863e2e6328bfeb
diprama <50134297+diprama@users.noreply.github.com>
2024-02-14 12:33:56 +0700

    Update booking_process.php

--- booking/booking_process.php
+++ booking/booking_process.php
@@ -281,7 +281,7 @@ Appointment Type: $txtJenis <br>";
 
       // Set informasi pengirim dan penerima
       $mail2->setFrom('official@pixify.id', 'Pixify');
-      $mail2->addAddress('sf.selfstudio@gmail.com'); // Ganti dengan alamat email tujuan
+      $mail2->addAddress('official@pixify.id'); // Ganti dengan alamat email tujuan
       // $mail2->addAddress('dickypramanasukma@gmail.com'); // Ganti dengan alamat email tujuan
 
 
