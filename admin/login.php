f6b407150fc46a56b4159f07af440034f977f0a2
diprama <50134297+diprama@users.noreply.github.com>
2024-02-25 14:09:19 +0700

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -11,8 +11,8 @@
     <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="PIXINVENT">
     <title>Pixify Studio - Login</title>
-    <link rel="apple-touch-icon" href="../app-assets/images/logo/pixify1.png">
-    <link rel="shortcut icon" type="image/x-icon" href="../app-assets/images/logo/pixify1.png">
+    <link rel="apple-touch-icon" href="../app-assets/images/logo/pixify.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../app-assets/images/logo/pixify.png">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
 
 
