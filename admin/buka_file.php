5846d930adf2482f83e15f533b39ae32f63d0f8c
diprama <50134297+diprama@users.noreply.github.com>
2024-02-25 15:13:01 +0700

    Update buka_file.php

--- admin/buka_file.php
+++ admin/buka_file.php
@@ -48,6 +48,11 @@ if ($_GET) {
       include "management_booking_update.php";
       break;
 
+    case 'Management-Booking-Process':
+      if (!file_exists("management_booking_process.php")) die("Sorry Empty Page!");
+      include "management_booking_process.php";
+      break;
+
     case 'Management-Booking-Process-Detail':
       if (!file_exists("management_booking_process_detail.php")) die("Sorry Empty Page!");
       include "management_booking_process_detail.php";
