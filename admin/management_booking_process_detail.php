ed5d7b624af02c1af102b89719427b6085fdf7b9
diprama <50134297+diprama@users.noreply.github.com>
2024-02-25 15:19:45 +0700

    Update management_booking_process_detail.php

--- admin/management_booking_process_detail.php
+++ admin/management_booking_process_detail.php
@@ -135,34 +135,116 @@ $id = $_GET['id'];
                             <label>Jam Booking*</label>
                             <select class="form-select" id="waktu" name="txtJam" aria-label="Default select example" autocomplete="off" required>
                               <?php
-                                  $mySql  = "SELECT * from jadwal j where j.status ='1' and j.availability ='0'  order by j.jam asc;";
-                    
-                                $myQry  = mysqli_query($koneksidb, $mySql)  or die("RENTAS ERP ERROR : " . mysqli_error($koneksidb));
-                                while ($myData = mysqli_fetch_array($myQry)) {
-                                  // set tanggal hari ini
-                                  $hariini = date('Y-m-d');
-                                    // jadwal jam yang tersedia
-                                    $jam = date("H:i", strtotime($myData['jam']));
-                                    $dataJam = date("H:i", strtotime($dataJam));
-                                      #jam sesuaikan dengan jam yang diset sebelumnya
-                                      if ($jam == $dataJam) {
-                                        $selected = "selected";
-                                      } else {
-                                        $selected = "";
-                                      }
-                                      ?>
-                                        <option <?=$selected ?> value="<?php echo $jam  ?>"><?php echo $jam ?></option>;
-                                    <?php
-                                    // jika tanggal yang dipilih bukan hari ini maka tampilkan semua 
-                                  
+                              $mySql  = "SELECT * from jadwal j where j.status ='1' and j.availability ='0'  order by j.jam asc;";
+
+                              $myQry  = mysqli_query($koneksidb, $mySql)  or die("RENTAS ERP ERROR : " . mysqli_error($koneksidb));
+                              while ($myData = mysqli_fetch_array($myQry)) {
+                                // set tanggal hari ini
+                                $hariini = date('Y-m-d');
+                                // jadwal jam yang tersedia
+                                $jam = date("H:i", strtotime($myData['jam']));
+                                $dataJam = date("H:i", strtotime($dataJam));
+                                #jam sesuaikan dengan jam yang diset sebelumnya
+                                if ($jam == $dataJam) {
+                                  $selected = "selected";
+                                } else {
+                                  $selected = "";
                                 }
-                            
+                              ?>
+                                <option <?= $selected ?> value="<?php echo $jam  ?>"><?php echo $jam ?></option>;
+                              <?php
+                                // jika tanggal yang dipilih bukan hari ini maka tampilkan semua 
+
+                              }
+
 
                               ?>
                             </select>
                           </div>
                         </div>
                       </div>
+
+                      <div class="card-datatable">
+                        <table class="table datatables-basic table-striped table-bordered dt-responsive " cellspacing="0" width="100%">
+                          <thead>
+                            <tr>
+                              <th>No</th>
+                              <th>Nama</th>
+                              <th>Hari</th>
+                              <th>Tanggal</th>
+                              <th>Jam</th>
+                              <th>No WA</th>
+                              <th>Paket</th>
+                              <th>Background</th>
+                              <th>Status</th>
+                              <th>Action</th>
+                              <!-- <th>Reschedule</th> -->
+                            </tr>
+                          </thead>
+                          <tbody>
+
+                            <?php
+                            $mySql   = "SELECT * FROM booking where status ='Dikonfirmasi' and tanggal >= '$tanggal_hari_ini' order by tanggal desc";
+                            $myQry   = mysqli_query($koneksidb, $mySql)  or die("ERROR BOOKING:  " . mysqli_error($koneksidb));
+                            $nomor  = 0;
+                            while ($myData = mysqli_fetch_array($myQry)) {
+                              $nomor++;
+                              $Code = $myData['id'];
+                              $Jam = $myData['jam'];
+
+                              // ganti format jam
+                              $Jam = $Jam;
+                              $Jam = date("G:i", strtotime($Jam));
+                              // set hari
+                              $tanggal = $myData['tanggal'];
+                              $hari = hari_ini($tanggal);
+
+                            ?>
+
+                              <tr>
+                                <td><?php echo $nomor; ?></td>
+                                <td><?php echo $myData['nama']; ?></td>
+                                <td><?php echo $hari; ?></td>
+                                <td><?php echo $myData['tanggal']; ?></td>
+                                <td><?php echo $Jam; ?></td>
+                                <td><?php echo $myData['no_wa']; ?></td>
+                                <td><?php echo $myData['paket']; ?></td>
+                                <td><?php echo $myData['background']; ?></td>
+                                <td><?php echo $myData['status']; ?></td>
+                                <?php if ($myData['status'] == 'Dikonfirmasi') { ?>
+                                  <td>
+                                    <div class="dropdown">
+                                      <button type="button" class="btn btn-sm dropdown-toggle hide-arrow py-0" data-bs-toggle="dropdown">
+                                        <i data-feather="more-vertical"></i>
+                                      </button>
+                                      <div class="dropdown-menu dropdown-menu-end">
+                                        <a class="dropdown-item" href="?page=Management-Booking-Process-Detail&id=<?php echo $Code; ?>" onclick="return confirm('INGIN KONFIRMASI DATA?')" role="button"><i class="fa fa-pencil fa-fw">
+                                            <i data-feather="check" class="me-50"></i>
+                                            <span>Selesai</span>
+                                        </a>
+                                        <a class="dropdown-item" href="?page=Management-Booking-Rescheduled&id=<?php echo $Code; ?>" onclick="return confirm('INGIN RESCHEDULED?')" role="button"><i class="fa fa-pencil fa-fw">
+                                            <i data-feather="edit-2" class="me-50"></i>
+                                            <span>Re-Schedule</span>
+                                        </a>
+                                        <a class="dropdown-item" href="?page=Management-Booking-Cancel&id=<?php echo $Code; ?>" onclick="return confirm('INGIN HAPUS DATA?')" role="button"><i class="fa fa-pencil fa-fw">
+                                            <i data-feather="trash" class="me-50"></i>
+                                            <span>Batalkan</span>
+                                        </a>
+                                      </div>
+                                    </div>
+                                  </td>
+                                <?php } else { ?>
+                                  <td></td>
+                                <?php } ?>
+                              </tr>
+                            <?php }
+                            ?>
+
+                          </tbody>
+                        </table>
+                      </div>
+
+
 
                     </div>
                     <div class="col-7 my-5">
