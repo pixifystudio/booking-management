df0dfc3009e5fe88bbe8a275e8c3d4c19ada1c37
diprama <50134297+diprama@users.noreply.github.com>
2024-02-25 14:08:33 +0700

    logo

--- admin/header_v2.php
+++ admin/header_v2.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
     <title>Pixify Studio Admin - <?= $_SESSION['SES_TITLE'] ?></title>
 
-    <link rel="apple-touch-icon" href="../app-assets/images/logo/pixify1.png">
-    <link rel="shortcut icon" type="image/x-icon" href="../app-assets/images/logo/pixify1.png">
+    <link rel="apple-touch-icon" href="../app-assets/images/logo/pixify.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../app-assets/images/logo/pixify.png">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
 
     <!-- BEGIN: Vendor CSS-->
