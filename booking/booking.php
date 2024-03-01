fa5fd68c623d02d3b7c3bd92e1d752399528f0fc
diprama <50134297+diprama@users.noreply.github.com>
2024-03-01 17:02:38 +0700

    Update booking.php

--- booking/booking.php
+++ booking/booking.php
@@ -290,7 +290,7 @@ $randomToken = generateRandomToken();
                                         // jika tanggal yang dipilih hari ini, set validasi
                                         if ($hariini == $txtTanggal) {
                                           // set jam sekarang tambah 1 jam
-                                          $jamsekarang = date("H:i", strtotime("+60 minutes"));
+                                          $jamsekarang = date("H:i", strtotime("+0 minutes"));
                                           // jadwal jam yang tersedia
                                           $jam = date("H:i", strtotime($myData['jam']));
                                           // tampilkan daftar jam minimal 1 jam dari jam sekarang
