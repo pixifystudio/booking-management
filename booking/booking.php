110806a1a2fbdba1df11fee55e5fc2bdbab52385
diprama <50134297+diprama@users.noreply.github.com>
2024-03-01 18:42:47 +0700

    Update booking.php

--- booking/booking.php
+++ booking/booking.php
@@ -30,12 +30,17 @@ if (isset($_POST['btnSubmit'])) {
     $txtTanggal = '';
   }
 
-   $jam_sekarang = date("G:i");
-  $jam_tutup = date("18:00");
+  // kalau yang dipilih tanggal nya hari ini, maka validasi jam jalan
+  if ($txtTanggal == $tanggal_sekarang) {
+    $jam_sekarang = date("G:i");
+    $jam_tutup = date("18:00");
+    // kalau jam yang dipilih melebihi jam tutup, validasi jalan
   if ($jam_sekarang > $jam_tutup) {
     echo "<meta http-equiv='refresh' content='0; url=?page=Booking-Closed'>";
     exit;
   }
+  }
+
 }
 
 
