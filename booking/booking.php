f669dcc0158f02e01fe65444feefbd889337c62e
diprama <50134297+diprama@users.noreply.github.com>
2024-03-24 12:14:47 +0700

    Update booking.php

--- booking/booking.php
+++ booking/booking.php
@@ -291,13 +291,14 @@ $randomToken = generateRandomToken();
                                       if ($nama_hari == 'Sunday' || $nama_hari == 'Saturday') {
                                         $mySql  = "SELECT * from jadwal j where j.status ='1' and j.availability ='0' and j.jam >='09:00' and j.jam <='17:00' and j.jam not in (select jam from booking where tanggal = '$txtTanggal') order by j.jam asc;";
                                       }
-                                      // tambah validasi
-                                      echo $txtTanggal;
-                                      else if ($txtTanggal=='2024-03-25' || $txtTanggal=='2024-03-26' ){
-                                        $mySql  = "SELECT * from jadwal j where j.status ='1' and j.availability ='0' and j.jam >='19:00' and j.jam <='22:00' and j.jam not in (select jam from booking where tanggal = '$txtTanggal') order by j.jam asc;";
-                                      } 
                                       else {
-                                        $mySql  = "SELECT * from jadwal j where j.status ='1' and j.availability ='0'  and j.jam >='09:00' and j.jam <='17:00' and j.jam not in (select jam from booking where tanggal = '$txtTanggal') order by j.jam asc;";
+                                        if ($txtTanggal =='2024-03-25') {
+                                          $mySql  = "SELECT * from jadwal j where j.status ='1' and j.availability ='0'  and j.jam >='19:00' and j.jam <='22:00' and j.jam not in (select jam from booking where tanggal = '$txtTanggal') order by j.jam asc;";
+                                        }
+                                        else {
+                                          $mySql  = "SELECT * from jadwal j where j.status ='1' and j.availability ='0'  and j.jam >='09:00' and j.jam <='17:00' and j.jam not in (select jam from booking where tanggal = '$txtTanggal') order by j.jam asc;";
+
+                                        }
                                       }
 
                                       $myQry  = mysqli_query($koneksidb, $mySql)  or die("RENTAS ERP ERROR : " . mysqli_error($koneksidb));
