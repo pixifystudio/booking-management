b355280b926bce898664e9ed14454a7311c88f38
diprama <50134297+diprama@users.noreply.github.com>
2024-04-13 06:28:19 +0700

    Update booking.php

--- booking/booking.php
+++ booking/booking.php
@@ -293,7 +293,7 @@ $randomToken = generateRandomToken();
                                       }
                                       else {
                                         if ($nama_hari == 'Friday' ) {
-                                          $mySql  = "SELECT * from jadwal j where j.status ='1' and j.availability ='0'  and j.jam >='13:00' and j.jam <'19:00' and j.jam not in ('17:20','17:40','18:00','18:20','18:40') and j.jam not in (select jam from booking where tanggal = '$txtTanggal') order by j.jam asc;";
+                                          $mySql  = "SELECT * from jadwal j where j.status ='1' and j.availability ='0'  and j.jam >='13:00' and j.jam <='21:00' and j.jam not in ('17:20','17:40','18:00','18:20','18:40') and j.jam not in (select jam from booking where tanggal = '$txtTanggal') order by j.jam asc;";
                                         }
                                         else {
                                           $mySql  = "SELECT * from jadwal j where j.status ='1' and j.availability ='0'  and j.jam >='11:00' and j.jam <='21:00' and j.jam not in (select jam from booking where tanggal = '$txtTanggal') order by j.jam asc;";
