c405750a2d8426f70c33dcbcfc09bff4b721338d
diprama <50134297+diprama@users.noreply.github.com>
2024-05-05 02:01:57 +0700

    Update get_background.php

--- booking/get_background.php
+++ booking/get_background.php
@@ -13,7 +13,7 @@ $jenis   = $Channel[0];
 $validasi = 'NO';
 $html = "";
 // validasi apakah mempunya sub part atau tidak
-$sql = mysqli_query($koneksidb, "SELECT * FROM master_background and where jenis='$jenis' ORDER BY id asc");
+$sql = mysqli_query($koneksidb, "SELECT * FROM master_background  where jenis='$jenis' ORDER BY id asc");
 // $cekQry = mysqli_query($koneksidb, $sql) or die("RENTAS ERP ERROR : " . mysqli_error($koneksidb));
 if (mysqli_num_rows($sql) >= 1) {
 	$validasi = 'YES';
