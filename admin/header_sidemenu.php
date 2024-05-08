3d2b1b48df6a39e69de608f37789ba72326f1e04
diprama <50134297+diprama@users.noreply.github.com>
2024-05-08 13:09:03 +0700

    Update header_sidemenu.php

--- admin/header_sidemenu.php
+++ admin/header_sidemenu.php
@@ -10,12 +10,7 @@
 
     <div class="horizontal-menu-wrapper">
         <div class="header-navbar navbar-expand-sm navbar navbar-horizontal floating-nav navbar-shadow menu-border" role="navigation" data-menu="menu-wrapper" data-menu-type="floating-nav">
-            <div class="navbar-header">
-                <ul class="nav navbar-nav flex-row">
-
-                    <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pe-0" data-bs-toggle="collapse"><i class="d-block d-xl-none text-primary toggle-icon font-medium-4" data-feather="x"></i></a></li>
-                </ul>
-            </div>
+           
             <div class="shadow-bottom"></div>
             <div class="navbar-container main-menu-content" data-menu="menu-container">
                 <ul class="nav navbar-nav" id="main-menu-navigation" data-menu="menu-navigation">
