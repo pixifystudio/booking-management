e3331a2833fea1419077a23c9b3d2854f7141496
diprama <50134297+diprama@users.noreply.github.com>
2024-05-15 18:27:57 +0700

    Update booking.php

--- booking/booking.php
+++ booking/booking.php
@@ -387,7 +387,7 @@ $randomToken = generateRandomToken();
 
                               <div class="col-lg-10" style="padding-top: 15px; padding-left: 10px;">
                                 <label for="email">Background*</label>
-                                <select class="form-select" id="background" style='width:80%' name="txtBackground" aria-label="Default select example" autocomplete="off" required>
+                                <select class="form-select" id="background" style='width:82%' name="txtBackground" aria-label="Default select example" autocomplete="off" required>
                                   <option selected value="">Pilih</option>
                                   <?php
                                   // panggil database
@@ -404,7 +404,7 @@ $randomToken = generateRandomToken();
                               <div class="col-lg-10" style="padding-top: 15px; padding-left: 10px;">
                                 <div class="form-group">
                                   <label>Email*</label>
-                                  <input class="form-control" style='width:80%' type="text" placeholder="masukkin alamat Email kamu" name="txtEmail" autocomplete="off" required>
+                                  <input class="form-control" style='width:82%' type="text" placeholder="masukkin alamat Email kamu" name="txtEmail" autocomplete="off" required>
                                 </div>
                               </div>
 
