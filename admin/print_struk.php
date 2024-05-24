a5774b81c4bbb4059405a583751eb9018f7eb46c
diprama <50134297+diprama@users.noreply.github.com>
2024-05-24 17:42:26 +0700

    Update print_struk.php

--- admin/print_struk.php
+++ admin/print_struk.php
@@ -20,7 +20,7 @@ class PDF extends FPDF
 $pdf = new PDF('P', 'mm', array(20, 30)); // P for Portrait, mm for millimeters, array(58, 30) for custom size
 
 // Add a new page
-$pdf->AddPage();
+// $pdf->AddPage();
 
 // Set font
 $pdf->SetFont('Arial', 'B', 12);
