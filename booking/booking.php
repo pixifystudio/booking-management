3f9669046942139ab42a3e1fb5a9f5e4a2a17b81
diprama <50134297+diprama@users.noreply.github.com>
2024-05-29 19:22:03 +0700

    Update booking.php

--- booking/booking.php
+++ booking/booking.php
@@ -509,9 +509,9 @@ $randomToken = generateRandomToken();
 
                                 <div class="button-container-calendar">
 
-                                  <button id="previous">&#8249;</button>
+                                  <div id="previous">&#8249;</div>
 
-                                  <button id="next">&#8250;</button>
+                                  <div id="next">&#8250;</div>
 
                                   <h3 id="monthHeader"></h3>
                                   <p id="yearHeader"></p>
