9c9ff9b49ccc9ef836ade738b51732939a924d6e
diprama <50134297+diprama@users.noreply.github.com>
2024-05-29 20:32:39 +0700

    Update print_struk.php

--- admin/print_struk.php
+++ admin/print_struk.php
@@ -87,6 +87,7 @@ $pdf->SetFont('Arial', 'B', 3.5); // Ukuran font disesuaikan agar sesuai dengan 
 $pdf->Cell(7, 6, '', '', 0, 'L', 0);
 $pdf->Cell(35, 6, 'Self Photo', '', 0, 'L', 0);
 $pdf->Cell(7, 6, '', '', 0, 'L', 0);
+$pdf->Ln(2);
 $pdf->SetFont('Arial', '', 3.5); // Ukuran font disesuaikan agar sesuai dengan ukuran kertas kecil
 $pdf->Cell(35, 6, '2 x Rp 50.000', '', 0, 'L', 0);
 $pdf->Cell(25, 6, 'Rp 60.000', '', 0, 'L', 0);
