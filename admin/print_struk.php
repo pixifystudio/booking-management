1252a3b8b3758d0197e29bc6e82394cb3b8ad0d7
diprama <50134297+diprama@users.noreply.github.com>
2024-05-29 23:18:51 +0700

    Update print_struk.php

--- admin/print_struk.php
+++ admin/print_struk.php
@@ -45,8 +45,8 @@ $x = $pdf->GetX();
 $y = $pdf->GetY();
 $pdf->Image('../app-assets/images/logo/pixifystudio.png', 17, 0, 15);
 $pdf->Image('../app-assets/images/logo/strukpixify.jpg', 20, 60, 8);
-$pdf->Image('../app-assets/images/logo/instagram.png', 27, 67, 1);
-$pdf->Image('../app-assets/images/logo/whatsapp.png', 14, 67, 1);
+$pdf->Image('../app-assets/images/logo/instagram.png', 23, 70, 1);
+$pdf->Image('../app-assets/images/logo/whatsapp.png', 13, 70, 1);
 $pdf->SetXY($x, $y);
 
 $pdf->Ln(10);
