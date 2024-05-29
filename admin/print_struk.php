33d54ac9160aecf48a5a83e145fa984d1777db98
diprama <50134297+diprama@users.noreply.github.com>
2024-05-29 23:05:34 +0700

    Update print_struk.php

--- admin/print_struk.php
+++ admin/print_struk.php
@@ -143,7 +143,7 @@ $pdf->Cell(45, 6, 'Kamu bisa share pengalaman foto kamu dengan cara scan QR beri
 $pdf->Ln(1.5);
 $pdf->Cell(45, 6, 'atau upload keseruannya di socmed dan tag akun kita ya!', '', 0, 'C', 0);
 
-$pdf->Ln(10);
+$pdf->Ln(12);
 
 $pdf->SetFont('Arial', 'B', 3.5); // Ukuran font disesuaikan agar sesuai dengan ukuran kertas kecil
 $pdf->Cell(45, 6, '0851-7121-2096       @PXY.STUDIO', '', 0, 'C', 0);
