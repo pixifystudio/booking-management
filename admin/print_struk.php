a24b1b50fedde33134bb2c807d60d8b41bf58595
diprama <50134297+diprama@users.noreply.github.com>
2024-06-07 21:32:39 +0700

    Update print_struk.php

--- admin/print_struk.php
+++ admin/print_struk.php
@@ -158,7 +158,7 @@ $pdf->Ln(8);
 
 $x = $pdf->GetX();
 $y = $pdf->GetY();
-$pdf->SetXY($x + 100, $y);
+$pdf->SetXY($x - 40, $y);
 $pdf->SetFont('Arial', 'B', 6); // Ukuran font disesuaikan agar sesuai dengan ukuran kertas kecil
 $pdf->Cell(45, 6,'', '', 0, 'L', 0);
 $pdf->Cell(45, 6, '0851-7121-2096             @PXY.STUDIO', '', 0, 'L', 0);
