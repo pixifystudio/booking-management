2ecf36b0b7b1731738439e4ad81e5c05a74306b6
diprama <50134297+diprama@users.noreply.github.com>
2024-06-07 23:04:45 +0700

    Update print_struk.php

--- admin/print_struk.php
+++ admin/print_struk.php
@@ -184,7 +184,7 @@ $pdf->Cell(10, 6, 'Rp. ' . number_format(($total - $dp)), '', 0, 'L', 0);
 $x = $pdf->GetX();
 $y = $pdf->GetY();
 $pdf->SetXY($x, $y + 10);
-
+$pdf->Ln(1.5);
 $pdf->SetFont('Arial', '', 4.3); // Ukuran font disesuaikan agar sesuai dengan ukuran kertas kecil
 $pdf->Cell(45, 6, 'Terimakasih sudah foto di Pixify Studio', '', 0, 'C', 0);
 $pdf->Ln(1.5);
