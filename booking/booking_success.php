88fbfad4dbe1562fd4c48d3a8303585ff36b17ea
diprama <50134297+diprama@users.noreply.github.com>
2024-06-15 18:46:01 +0700

    Update booking_success.php

--- booking/booking_success.php
+++ booking/booking_success.php
@@ -157,13 +157,14 @@ $username = isset($myData['username']) ? $myData['username'] : '';
                         <form id="login_form" method="POST">
                           <div class="title-main align-left" style="padding-left: 10px">
                             <p>Terimakasih <?php echo $nama ?></p>
-                            <p>Selanjutnya yang harus kamu lakukan:</p>
-                            <ol style="margin-left: 20px; line-height: 1.8;">
+                            <p>Se
+                              lanjutnya yang harus kamu lakukan:</p>
+                            <ol style="margin-left: 30px; line-height: 1.8;">
                               <li>Cek email kamu untuk detail pemesanan</li>
                               <li>Melakukan pembayaran DP sebesar Rp20.000 melalui rekening BRI 409101008799535 a.n Dicky Pramana Sukma</li>
                               <li>Kirim bukti pembayaran melalui no WhatsApp: 08517121209</li>
                             </ol>
-                            <p>Sampai berjumpa di Self Studio :)</p>
+                            <p>Sampai berjumpa di Pixify Studio :)</p>
                           </div>
 
                         </form>
