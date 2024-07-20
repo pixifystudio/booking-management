a594345222052eaf21b1ec9e0d048ff481a41300
diprama <50134297+diprama@users.noreply.github.com>
2024-07-21 00:18:44 +0700

    Update config.js

--- booking/js3/chaindropdown/config.js
+++ booking/js3/chaindropdown/config.js
@@ -1,39 +1,39 @@
-$(document).ready(function(){ // Ketika halaman sudah siap (sudah selesai di load)
+$(document).ready(function () { // Ketika halaman sudah siap (sudah selesai di load)
 	// Kita sembunyikan dulu untuk loadingnya
 	$("#loading").hide();
 
 
-	
-	$("#jenisfoto").change(function(){ // Ketika user mengganti atau memilih data kategori
+
+	$("#jenisfoto").change(function () { // Ketika user mengganti atau memilih data kategori
 		// $("#subkategori").hide(); // Sembunyikan dulu combobox subkategori nya
 		// // $("#loading").show(); // Tampilkan loadingnya
 		// $("#subkategoriform").show(); // munculkan box subkategori
-		$('#background').attr('disabled', false);
-		
-	
+		$('#paket').attr('disabled', false);
+
+
 		$.ajax({
 			type: "POST", // Method pengiriman data bisa dengan GET atau POST
-			url: "get_background.php", // Isi dengan url/path file php yang dituju
-			data: { kategori: $("#jenisfoto").val()}, // data yang akan dikirim ke file yang dituju
+			url: "get_pilihan.php", // Isi dengan url/path file php yang dituju
+			data: { kategori: $("#jenisfoto").val() }, // data yang akan dikirim ke file yang dituju
 			dataType: "json",
-			beforeSend: function(e) {
-				if(e && e.overrideMimeType) {
+			beforeSend: function (e) {
+				if (e && e.overrideMimeType) {
 					e.overrideMimeType("application/json;charset=UTF-8");
 				}
 			},
-			success: function(response){ // Ketika proses pengiriman berhasil
+			success: function (response) { // Ketika proses pengiriman berhasil
 				// $("#loading").hide(); // Sembunyikan loadingnya
 
 				// set isi dari combobox subkategori
 				// lalu munculkan kembali combobox subkategorinya
-				$("#background").html(response.data_subkategori).show();
+				$("#paket").html(response.data_subkategori).show();
 			},
 			error: function (xhr, ajaxOptions, thrownError) { // Ketika ada error
 				alert(thrownError); // Munculkan alert error
 			}
 		});
 	});
-	
+
 });
 
 
