ace142ab9ad387dd9e2e5fe7cbde592d1c1fdf94
diprama <50134297+diprama@users.noreply.github.com>
2024-08-19 01:29:18 +0700

    Update header_v2.php

--- admin/header_v2.php
+++ admin/header_v2.php
@@ -70,7 +70,7 @@ include "language.php";
     function rupiah($angka)
     {
 
-        $hasil_rupiah = "Rp " . number_format($angka, 2, ',', '.');
+        $hasil_rupiah = "Rp " . number_format($angka, 0, ',', '.');
         return $hasil_rupiah;
     }
 
