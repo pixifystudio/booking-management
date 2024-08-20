5094e062f62432fe303b7e862492b6034cac523e
diprama <50134297+diprama@users.noreply.github.com>
2024-08-20 15:29:58 +0700

    Update test_booking_process.php

--- booking/test_booking_process.php
+++ booking/test_booking_process.php
@@ -24,17 +24,17 @@ if (isset($_POST['btnSubmit'])) {
   $txtTanggal   = $_POST['txtTanggal'];
   // ganti format tanggal
   $originalDate = $txtTanggal;
-   $txtTanggal = date("Y-m-d", strtotime($originalDate));
-   $txtWaktu = $_POST['txtWaktu'];
+  $txtTanggal = date("Y-m-d", strtotime($originalDate));
+  $txtWaktu = $_POST['txtWaktu'];
   $txtJenis = $_POST['txtJenis'];
   $txtPaket = $_POST['txtPaket'];
   $txtBackground = $_POST['txtBackground'];
   $txtNama = $_POST['txtNama'];
   $txtEmail = $_POST['txtEmail'];
   $txtWhatsapp = $_POST['txtWhatsapp'];
-  $txtInstagram = isset($_POST['txtInstagram']) ? $_POST['txtInstagram']:'';
+  $txtInstagram = isset($_POST['txtInstagram']) ? $_POST['txtInstagram'] : '';
   $txtStatus = 'Dibuat';
-   $txtToken = $_POST['txtToken'];
+  $txtToken = $_POST['txtToken'];
 
   // ambil nama hari ini
   function hari_ini()
@@ -79,6 +79,7 @@ if (isset($_POST['btnSubmit'])) {
     return "<b>" . $hari_ini . "</b>";
   }
 
+  // ambil nama hari ini
   function bulan_ini()
   {
     $txtTanggal = $_POST['txtTanggal'];
@@ -143,7 +144,6 @@ if (isset($_POST['btnSubmit'])) {
 
   // set ke variabel 
   $hari_ini = hari_ini();
-  // set ke variabel 
   $bulan_ini = bulan_ini();
   $tanggal_ini = date("j", strtotime($txtTanggal));
   $tahun_ini = date("Y", strtotime($txtTanggal));
@@ -151,21 +151,22 @@ if (isset($_POST['btnSubmit'])) {
   $date_ini = $tanggal_ini . ' ' . $bulan_ini . ' ' . $tahun_ini;
 
 
-  // cek apakah detail transaksi dengan token yang diset sudah ada di database
-    $mySql  = "SELECT token from test_booking where token ='$txtToken'";
-    $myQry  = mysqli_query($koneksidb, $mySql)  or die("RENTAS ERP ERROR : " . mysqli_error($koneksidb));
-    $JumlahData = mysqli_num_rows($myQry);
 
-    // jalankan proses submit jika data = 0
-    if ($JumlahData >= 1) {
+  // cek apakah detail transaksi dengan token yang diset sudah ada di database
+  $mySql  = "SELECT token from booking where token ='$txtToken'";
+  $myQry  = mysqli_query($koneksidb, $mySql)  or die("RENTAS ERP ERROR : " . mysqli_error($koneksidb));
+  $JumlahData = mysqli_num_rows($myQry);
+
+  // jalankan proses submit jika data = 0
+  if ($JumlahData >= 1) {
     $pesanError[] = "Tidak good";
-    }
+  }
 
   # JIKA ADA PESAN ERROR DARI VALIDASI
   if (count($pesanError) >= 1) {
 
     // ambil data sesuai token yang sudah ada di set
-    $mySql  = "SELECT id, token from test_booking where token ='$txtToken'";
+    $mySql  = "SELECT id, token from booking where token ='$txtToken'";
     $myQry  = mysqli_query($koneksidb, $mySql)  or die("RENTAS ERP ERROR : " . mysqli_error($koneksidb));
     $Ambildata = mysqli_fetch_array($myQry);
 
@@ -179,44 +180,66 @@ if (isset($_POST['btnSubmit'])) {
     // echo "</div>";
 
     // Tampilkan booking success tanpa submit
-    echo "<meta http-equiv='refresh' content='0; url=?page=Test-Booking-Success&id=$last_id'>";
-
-
+    echo "<meta http-equiv='refresh' content='0; url=?page=Booking-Success&id=$last_id'>";
   } else {
     # INSERT KE DATABASE BOOKING DAN KIRIM EMAIL
 
-      $mySql = "INSERT INTO `test_booking`( `nama`, `email`, `no_wa`, `jenis`, `paket`, `background`, `instagram`, `tanggal`, `jam`, `status`, `updated_date`,`token`) VALUES 
+
+
+    $mySql = "INSERT INTO `booking`( `nama`, `email`, `no_wa`, `jenis`, `paket`, `background`, `instagram`, `tanggal`, `jam`, `status`, `updated_date`,`token`) VALUES 
       ('$txtNama','$txtEmail','$txtWhatsapp','$txtJenis','$txtPaket','$txtBackground','$txtInstagram','$txtTanggal','$txtWaktu','$txtStatus', now(),'$txtToken') ";
     $myQry = mysqli_query($koneksidb, $mySql) or die("Query Insert Salah : " . mysqli_error($koneksidb));
-    $last_id = $koneksidb->insert_id;
+
+    #ambil harga
+    $mySqlPrice = "SELECT * FROM `product_price` where `name` = '$txtPaket' ORDER BY id DESC LIMIT 1";
+    $myQryPrice = mysqli_query($koneksidb, $mySqlPrice) or die("Query Insert Salah : " . mysqli_error($koneksidb));
+    $DataPrice = mysqli_fetch_array($myQryPrice);
+
+    $txtNominal = $DataPrice['price'];
+
+
+
+    // ambil id terakhir
+    $mySqlID = "SELECT * FROM `booking` ORDER BY id DESC LIMIT 1";
+    $myQryID = mysqli_query($koneksidb, $mySqlID) or die("Query Insert Salah : " . mysqli_error($koneksidb));
+    $DataID = mysqli_fetch_array($myQryID);
+
+    $id_terakhir = $DataID['id'];
+
+    #detail
+    $mySql = "INSERT INTO `booking_detail`( `booking_id`, `item`, `nominal`, `updated_date`,`updated_by`) VALUES 
+      ('$id_terakhir','$txtPaket','$txtNominal', now(),'$txtNama') ";
+    $myQry = mysqli_query($koneksidb, $mySql) or die("Query Insert Salah : " . mysqli_error($koneksidb));
+
     // Kirim email customer
     // Inisialisasi PHPMailer
     $mail = new PHPMailer(true);
 
-    $urlcancel = "https://sf-selfstudio.com/booking/?page=Test-Booking-Cancel&id=" . $last_id;
+    $urlcancel = "https://pixify.id/booking/?page=Booking-Cancel&id=" . $id_terakhir;
+
 
     try {
       // Set pengaturan SMTP
       $mail->isSMTP();
       $mail->Host = 'smtp.hostinger.com'; // Ganti dengan alamat SMTP server Anda
       $mail->SMTPAuth = true;
-      $mail->Username = 'official@sf-selfstudio.com'; // Ganti dengan username SMTP Anda
-      $mail->Password = 'SELFstudio123!'; // Ganti dengan password SMTP Anda
+      $mail->Username = 'official@pixify.id'; // Ganti dengan username SMTP Anda
+      $mail->Password = 'ForwardDigiCraft@123'; // Ganti dengan password SMTP Anda
       $mail->SMTPSecure = 'TLS';
       $mail->Port = 587;
 
       // Set informasi pengirim dan penerima
-      $mail->setFrom('official@sf-selfstudio.com', 'Self Studio');
+      $mail->setFrom('official@pixify.id', 'Pixify');
       $mail->addAddress($txtEmail); // Ganti dengan alamat email tujuan
 
 
       // set lokasi template email
-      $template_file = "test_email_customer.php";
+      $template_file = "email_customer.php";
       // cek template nya ready atau tidak
       if (file_exists($template_file))
-      $email_message = file_get_contents($template_file);
+        $email_message = file_get_contents($template_file);
       else
-      die("Unable to locate your template file");
+        die("Unable to locate your template file");
 
       $formfields = "Nama: $txtNama <br>
 Pilihan Paket: $txtPaket <br>
@@ -224,7 +247,7 @@ Alamat Email: $txtEmail <br>
 No WhatsApp: $txtWhatsapp <br>
 Background: $txtBackground <br>
 Username Instagram: $txtInstagram <br>
-Appointment Type: $txtJenis <br>";
+Jenis Foto: $txtJenis <br>";
 
       // set variable 
       $swap_var = array(
@@ -232,13 +255,14 @@ Appointment Type: $txtJenis <br>";
         "{EMAIL_LOGO}" => "uploads/NEX.png",
         "{EMAIL_TITLE}" => "Notification",
         "{CUSTOM_URL}" => "https://www.heytuts.com/web-dev/php/send-emails-with-php-from-localhost-with-sendmail",
-        "{CUSTOM_IMG}" => "./assets/images/logo-sf.png",
+        "{CUSTOM_IMG}" => "https://i1.wp.com/www.heytuts.com/wp-content/uploads/2019/05/thumbnail_Send-emails-with-php-from-localhost-with-SendMail.png",
         "{NAME}" => 'Hi ' . $txtNama,
         "{DATE}" =>  $date_ini,
         "{TIME}" =>  $txtWaktu,
         "{HARI}" =>  $hari_ini,
         "{FORMFIELDS}" => $formfields,
         "{URLTIKETV}" => $urlcancel
+
       );
 
       // ngecek variable dan timpah dengan variable yang di cek , seperti SITE_ADDR, {NAME}, {lOGO}, {CUSTOM_URL} etc
@@ -252,7 +276,7 @@ Appointment Type: $txtJenis <br>";
       // Set informasi email
       $mail->isHTML(true);
       $mail->MsgHTML($email_message);
-      $mail->Subject = 'SF Self Photo Studio Booking';
+      $mail->Subject = 'Booking Kamu Telah Dikonfirmasi!';
 
       // Kirim email
       $mail->send();
@@ -273,15 +297,15 @@ Appointment Type: $txtJenis <br>";
       $mail2->isSMTP();
       $mail2->Host = 'smtp.hostinger.com'; // Ganti dengan alamat SMTP server Anda
       $mail2->SMTPAuth = true;
-      $mail2->Username = 'official@sf-selfstudio.com'; // Ganti dengan username SMTP Anda
-      $mail2->Password = 'SELFstudio123!'; // Ganti dengan password SMTP Anda
+      $mail2->Username = 'official@pixify.id'; // Ganti dengan username SMTP Anda
+      $mail2->Password = 'ForwardDigiCraft@123'; // Ganti dengan password SMTP Anda
       $mail2->SMTPSecure = 'TLS';
       $mail2->Port = 587;
 
       // Set informasi pengirim dan penerima
-      $mail2->setFrom('official@sf-selfstudio.com', 'Self Studio');
-      // $mail2->addAddress('sf.selfstudio@gmail.com'); // Ganti dengan alamat email tujuan
-      $mail2->addAddress('dickypramanasukma@gmail.com'); // Ganti dengan alamat email tujuan
+      $mail2->setFrom('official@pixify.id', 'Pixify');
+      $mail2->addAddress('pixify.capture@gmail.com'); // Ganti dengan alamat email tujuan
+      // $mail2->addAddress('dickypramanasukma@gmail.com'); // Ganti dengan alamat email tujuan
 
 
 
@@ -289,9 +313,9 @@ Appointment Type: $txtJenis <br>";
       $template_file = "email_admin.php";
       // cek template nya ready atau tidak
       if (file_exists($template_file))
-      $email_message = file_get_contents($template_file);
+        $email_message = file_get_contents($template_file);
       else
-      die("Unable to locate your template file");
+        die("Unable to locate your template file");
 
       $formfields = "Nama: $txtNama <br>
 Pilihan Paket: $txtPaket <br>
@@ -299,7 +323,7 @@ Alamat Email: $txtEmail <br>
 No WhatsApp: $txtWhatsapp <br>
 Background: $txtBackground <br>
 Username Instagram: $txtInstagram <br>
-Appointment Type: $txtJenis <br>";
+Jenis Foto: $txtJenis <br>";
 
       // set variable 
       $swap_var = array(
@@ -336,14 +360,12 @@ Appointment Type: $txtJenis <br>";
     }
 
     // ambil id nya
-     $last_id = mysqli_insert_id($koneksidb);
-   # Validasi Insert Sukses
+    $last_id = mysqli_insert_id($koneksidb);
+    # Validasi Insert Sukses
     if ($myQry) {
-      echo "<meta http-equiv='refresh' content='0; url=?page=Test-Booking-Success&id=$last_id'>";
-    }
-    else {
+      echo "<meta http-equiv='refresh' content='0; url=?page=Booking-Success&id=$id_terakhir'>";
+    } else {
       echo "Query Gagal";
     }
- 
   }
 } // End POST
