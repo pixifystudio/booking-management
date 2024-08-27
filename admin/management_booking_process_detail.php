616415a68266704cb7eccbd4d7933bdc9522c86e
diprama <50134297+diprama@users.noreply.github.com>
2024-08-27 17:55:40 +0700

    Update management_booking_process_detail.php

--- admin/management_booking_process_detail.php
+++ admin/management_booking_process_detail.php
@@ -32,7 +32,7 @@ $id = $_GET['id'];
     // jika 
     if ($dataType =='inventory') {
       $mySql1   = "INSERT INTO `master_product_stock`( `product_id`,`stock`,`updated_date`)
-     VALUES ('$dataProduct','$dataQty',now())";
+     VALUES ('$dataProduct','-$dataQty',now())";
       $myQry1   = mysqli_query($koneksidb, $mySql1)  or die("ERROR INPUT STOCK:  " . mysqli_error($koneksidb));
     }
 
