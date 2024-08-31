fa900844e9b7bb5f1877167608b6f5b90de1e076
diprama <50134297+diprama@users.noreply.github.com>
2024-08-31 18:41:11 +0700

    Update management_booking_process_detail.php

--- admin/management_booking_process_detail.php
+++ admin/management_booking_process_detail.php
@@ -155,7 +155,7 @@ $id = $_GET['id'];
                         <div class="col-md-3 col-12">
                           <div class="form-group">
                             <label>Product</label>
-                            <select class="select2 form-select select2-hidden-accessible" name="txtProduct" aria-label="Default select example" autocomplete="off" required>
+                            <select class="select2 form-select" name="txtProduct" aria-label="Default select example" autocomplete="off" required>
                               <option selected value="">Pilih</option>
                               <?php
                               // deklarasi selected
