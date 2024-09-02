a108c1882b3d673430de57fe0505b52ca4afe68d
diprama <50134297+diprama@users.noreply.github.com>
2024-09-02 19:13:44 +0700

    Update management_booking_qr_add.php

--- admin/management_booking_qr_add.php
+++ admin/management_booking_qr_add.php
@@ -95,9 +95,7 @@ $_SESSION['SES_PAGE'] = "?page=Management-Booking-Rescheduled";
   <div class="header-navbar-shadow">
   </div>
   <div class="content-wrapper">
-    <div class="content-header r
-    ]/ ? .nw">
-      >.
+    <div class="content-header">
       <div class="content-header-left col-md-9 col-12 mb-2">
         <div class="row breadcrumbs-top">
           <div class="col-12">
