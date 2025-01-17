442846ff220286ebb2e35e581c16c3f35cdb6c26
diprama <50134297+diprama@users.noreply.github.com>
2025-01-17 20:29:03 +0700

    ganti database

--- admin/library/inc.connection.php
+++ admin/library/inc.connection.php
@@ -1,9 +1,9 @@
 <?php
 # Konek ke Web Server Lokal
 $myHost	= "127.0.0.1";
-$myUser	= "u480352240_sf_selfstudio";
+$myUser	= "u480352240_pixify";
 $myPass	= "ForwardDigiCraft@123";
-$myDbs	= "u480352240_sf_selfstudio"; // nama database, disesuaikan dengan database di MySQL
+$myDbs	= "u480352240_pixify"; // nama database, disesuaikan dengan database di MySQL
 
 # Konek ke Web Server Lokal
 $koneksidb	= mysqli_connect($myHost, $myUser, $myPass);
@@ -13,7 +13,7 @@ if (! $koneksidb) {
 
 
 # Memilih database pd MySQL Server
-mysqli_select_db($koneksidb, "u480352240_sf_selfstudio") or die ("Database not Found !");
+mysqli_select_db($koneksidb, "u480352240_pixify") or die ("Database not Found !");
 
 $link ="https://sf-selfstudio.com/";
 
