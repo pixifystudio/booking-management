7322854678c1c42cbd8e9edf9fb9893ce6a70745
diprama <50134297+diprama@users.noreply.github.com>
2025-02-15 00:10:28 +0700

    update

--- admin/validasi.php
+++ admin/validasi.php
@@ -6,11 +6,10 @@ $date  = isset($_POST['txtDate']) ?  $_POST['txtDate'] : '';
 $date2  = isset($_POST['txtDate2']) ?  $_POST['txtDate2'] : '';
 $datefrom  = isset($_POST['txtFrom']) ?  $_POST['txtFrom'] : '';
 $dateto  = isset($_POST['txtTo']) ?  $_POST['txtTo'] : '';
-echo $txtMonth  = isset($_POST['txtMonth']) ?  $_POST['txtMonth'] : '';
+ $txtMonth  = isset($_POST['txtMonth']) ?  $_POST['txtMonth'] : '';
 $txtYear  = isset($_POST['txtYear']) ?  $_POST['txtYear'] : '';
 $txtMetode  = isset($_POST['txtMetode']) ?  $_POST['txtMetode'] : '';
 
-exit;
 
 if (isset($_POST['btnHistory'])) {
   echo "<meta http-equiv='refresh' content='0; url=?page=Management-History&from=" . $datefrom  . "&to=" . $dateto  . "&p=" . $Paket   . "&b=" . $Background  . "'>";
