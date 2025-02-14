75f9fa6bf3ca0d3e932be25add07c0448efce2de
diprama <50134297+diprama@users.noreply.github.com>
2025-02-14 22:05:53 +0700

    Update management_booking_qr_add.php

--- admin/management_booking_qr_add.php
+++ admin/management_booking_qr_add.php
@@ -11,8 +11,7 @@ $id = isset($_GET['id']) ? $_GET['id'] : '';
 
 <!-- select2 -->
 
-<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
 
 <div class="app-content content ">
   <?php
@@ -281,7 +280,8 @@ $id = isset($_GET['id']) ? $_GET['id'] : '';
 </div>
 </div>
 <!-- END: Content-->
-
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script>
   $(document).ready(function() {
     $('.js-example-basic-single').select2();
