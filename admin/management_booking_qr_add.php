3ec95fd76755d9d5848ed632f090d4196dcf7ecb
diprama <50134297+diprama@users.noreply.github.com>
2025-02-14 22:08:17 +0700

    Update management_booking_qr_add.php

--- admin/management_booking_qr_add.php
+++ admin/management_booking_qr_add.php
@@ -280,14 +280,17 @@ $id = isset($_GET['id']) ? $_GET['id'] : '';
 </div>
 </div>
 <!-- END: Content-->
+
+<?php
+include "footer_v2.php";
+?>
+
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+
 <script>
   $(document).ready(function() {
     $('.js-example-basic-single').select2();
 });
 </script>
-
-<?php
-include "footer_v2.php";
-?>
