34f54069287c1ea6751e8e925d51c857561f50df
diprama <50134297+diprama@users.noreply.github.com>
2025-03-01 04:52:34 +0700

    Update kas.php

--- admin/kas.php
+++ admin/kas.php
@@ -14,10 +14,10 @@ $_SESSION['SES_PAGE'] = "?page=Management Admin";
       <div class="content-header-left col-md-9 col-12 mb-2">
         <div class="row breadcrumbs-top">
           <div class="col-12">
-            <h2 class="content-header-title float-start mb-0">Management Booking</h2>
+            <h2 class="content-header-title float-start mb-0">Finance</h2>
             <div class="breadcrumb-wrapper">
               <ol class="breadcrumb">
-                <li class="breadcrumb-item active"><a>Struk</a>
+                <li class="breadcrumb-item active"><a>Kas - Dashboard </a>
                 </li>
               </ol>
             </div>
