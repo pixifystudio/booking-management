f317e1d9363736a1e9a1798822bece1dbaa1764f
diprama <50134297+diprama@users.noreply.github.com>
2025-03-13 14:29:21 +0700

    nama konsumen

--- admin/management_booking_process_print.php
+++ admin/management_booking_process_print.php
@@ -29,15 +29,16 @@ $myQry = mysqli_query($koneksidb, $mySql) or die("ERROR BOOKING: " . mysqli_erro
 
     $NominalDP = isset($DataDP['dp']) ? $DataDP['dp'] :0;
     $MetodePembayaranDP = $DataDP['metode_pembayaran_dp'];
+    $NamaKonsumen = $DataDP['nama'];
 
     if ($NominalDP  >0) {
       // keluarkan nominal sesuai nominal DP
     $mySql1   = "INSERT INTO `transaction`( `keterangan`,`nominal`,`qty`,`metode`,`booking_detail_id`, `status`,`updated_date`)
-     VALUES ('Convert DP Booking ID : $txtID','$NominalDP','1','$txtMetode','$id','OUT',now())";
+     VALUES ('Convert DP Nama Konsumen : $NamaKonsumen','$NominalDP','1','$txtMetode','$id','OUT',now())";
     $myQry1   = mysqli_query($koneksidb, $mySql1)  or die("ERROR INPUT TRANSACTION:  " . mysqli_error($koneksidb));
 
      $mySql1   = "INSERT INTO `transaction`( `keterangan`,`nominal`,`qty`,`metode`,`booking_detail_id`, `status`,`updated_date`)
-     VALUES ('Convert DP Booking ID : $txtID','$NominalDP','1','$MetodePembayaranDP','$id','IN',now())";
+     VALUES ('Convert DP Nama Konsumen : $NamaKonsumen','$NominalDP','1','$MetodePembayaranDP','$id','IN',now())";
     $myQry1   = mysqli_query($koneksidb, $mySql1)  or die("ERROR INPUT TRANSACTION:  " . mysqli_error($koneksidb));
     }
 
